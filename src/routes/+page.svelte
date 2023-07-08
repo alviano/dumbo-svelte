8ecccb5fafa4d24a13d1fb8cf23ff7de2870386e
Mario Alviano <mario.alviano@gmail.com>
2023-07-08 11:02:42 +0200

    update packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,43 @@
 <script>
     import '../app.css';
-    import {Button, Input} from "sveltestrap";
-    import {DOMPurifyConfig, Utils} from "$lib/utils";
-    import {keydown} from "$lib/stores";
-    import Keybindings from "$lib/Keybindings.svelte";
-
-    $keydown.push((event) => {
-        console.log(event);
-        return false;
-    });
+    import {Utils} from "$lib/utils";
+    import {Base64} from "js-base64";
+    import pako from "pako";
 
 
+    const foo = `
+10 ─  LONG PIPE
+11 │  LONG PIPE
+20 ╮  CORNER TUBE
+21 ╯  CORNER TUBE
+22 ╰  CORNER TUBE
+23 ╭  CORNER TUBE
+30 ╶  CLOSED PIPE
+31 ╷  CLOSED PIPE
+32 ╴  CLOSED PIPE
+33 ╵  CLOSED PIPE
+50 ┬  T PIPE
+51 ┤  T PIPE
+52 ┴  T PIPE
+53 ├  T PIPE
+    `.trim();
+    const compressed = Utils.compress(foo);
+    const decompressed = Utils.uncompress(compressed);
+    console.log(foo)
+    console.log(decompressed)
+
+
+    const json = JSON.parse('{"input":"a.\\n§","encode_input":true,"decode_output":false,"recipe":[]}');
+    const base64data = Utils.compress(json);
+    const binData = Base64.toUint8Array(base64data);
+    const encoded = pako.inflate(binData);
+    console.log(binData)
+    console.log(encoded)
+
+    // console.log(binData)
+    // console.log(encoded)
+    // const json = String.fromCharCode.apply(null, [...new Uint16Array(encoded.buffer)]);
+    // return JSON.parse(json);
 
 </script>
-
-<Keybindings />
-
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<Button on:click={() => {Utils.snackbar('ok')}}>press me</Button>
-
-{@html Utils.render_markdown("_foo_", new DOMPurifyConfig({
-    PURIFY_ALLOWED_TAGS: ['p', 'em', '#text']
-}))}
-
-{@html Utils.render_markdown(`
-\`\`\`python
-print("hello")
-\`\`\`
-
-\`\`\`js
-let x = 5;
-\`\`\`
-
-\`\`\`c
-int x = 5;
-\`\`\`
-
-
-`, new DOMPurifyConfig({
-  PURIFY_LOG_ERRORS: 'true',
-  PURIFY_EXTERNAL_LINK_PREFIXES: [
-    'https://ssd2020.pythonanywhere.com/',
-    'https://cod.alviano.org/',
-  ],
-  PURIFY_ALLOWED_TAGS: 'a blockquote body br code em h1 h2 h3 h4 h5 h6 hr img li ol p pre span strong table tbody td th thead tr ul #text'.split(' '),
-  PURIFY_ALLOWED_ATTR: 'alt class href src style title'.split(' '),
-}))}
+asd
